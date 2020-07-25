1b885cf72325e08e1d3a0ffe3b7eaa22db50574c
Rafał Wójcik <rafi.incognito@o2.pl>
2020-07-25 13:12:25 +0200

    span html and currentValue js

--- src/js/index.js
+++ src/js/index.js
@@ -11,9 +11,21 @@ const textarea = document.querySelector('.textarea--js');
 const load = document.querySelector('.load--js');
 const save = document.querySelector('.save--js');
 
+
+const currentValue = localStorage.getItem('entry');
+
+if (currentValue) {
+    document.querySelector('.info--js').innerHTML = '!';
+}
+
 save.addEventListener('click', (e) => {
     e.preventDefault();
     localStorage.setItem('entry', textarea.value);
+    if (textarea.value) {
+        document.querySelector(".info--js").innerHTML = "!";
+    }else {
+        document.querySelector(".info--js").innerHTML = "";
+    }
 });
 
 
